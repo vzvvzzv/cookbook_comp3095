28d30247a54b42a239a6cb9fcd7340194f5a2c3b
Chi Calvin Nguyen <chicalvin.nguyen@georgebrown.ca>
2021-11-02 02:56:24 -0400

    calvin-redo_branch commit #3: added index file & controller recipe & user model

---
+++ cookbook/cookbook-data/src/main/java/model/Recipe.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Recipe {
+
+    private int id;
+    private String recipeName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+}
