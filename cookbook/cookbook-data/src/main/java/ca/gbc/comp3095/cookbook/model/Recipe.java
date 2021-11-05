fbff04390dac1a2a1226429159d0641632fe3ffc
coolatent <usurpertime@gmail.com>
2021-11-04 20:43:47 -0400

    Updated recipe.java to include more properties i.e. ingredients, servesHowMany. Started a bit on PlanMeal in advance with @Scheduling annotation and using cron property

--- cookbook/cookbook-data/src/main/java/ca/gbc/comp3095/cookbook/model/Recipe.java
+++ cookbook/cookbook-data/src/main/java/ca/gbc/comp3095/cookbook/model/Recipe.java
@@ -1,6 +1,7 @@
 package ca.gbc.comp3095.cookbook.model;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "recipes")
@@ -9,8 +10,15 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String recipename;
+    private String recipeName;
+    @Lob
+    //annotation specifies that the database should store the property as Large Object
+    private String ingredients;
+    @Lob
+    private String instructions;
+    private String servesHowMany;
+    private String cookAndPrepTime;
+    private Date creationDate;
 
     public Long getId() {
         return id;
@@ -20,11 +28,51 @@ public class Recipe {
         this.id = id;
     }
 
-    public String getRecipename() {
-        return recipename;
+    public String getRecipeName() {
+        return recipeName;
     }
 
-    public void setRecipename(String recipename) {
-        this.recipename = recipename;
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getServesHowMany() {
+        return servesHowMany;
+    }
+
+    public void setServesHowMany(String servesHowMany) {
+        this.servesHowMany = servesHowMany;
+    }
+
+    public String getCookAndPrepTime() {
+        return cookAndPrepTime;
+    }
+
+    public void setCookAndPrepTime(String cookAndPrepTime) {
+        this.cookAndPrepTime = cookAndPrepTime;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
     }
 }
