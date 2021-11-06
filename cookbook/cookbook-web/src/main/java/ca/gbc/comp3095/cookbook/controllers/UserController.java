6a8f7e3db24dce705563c2f1395987a6e6a7aefa
CC-Nguyen <chicalvin.nguyen@georgebrown.ca>
2021-11-06 14:41:19 -0400

    Calvin Commit #2: Cleaned & Commented Controllers (Except Recipe)

--- cookbook/cookbook-web/src/main/java/ca/gbc/comp3095/cookbook/controllers/UserController.java
+++ cookbook/cookbook-web/src/main/java/ca/gbc/comp3095/cookbook/controllers/UserController.java
@@ -1,3 +1,12 @@
+/*********************************************************************************
+ * Project: Cookbook App
+ * Assignment: COMP3095 Assignment1
+ * Author(s): Chi Calvin Nguyen, Simon Ung, Deniz Dogan
+ * Student Number: 101203877, 101032525, 101269485
+ * Date: 2021-11-06
+ * Description: UserController displays pages in the /users subdirectory.
+ * UserController manages the login and logout process
+ *********************************************************************************/
 package ca.gbc.comp3095.cookbook.controllers;
 
 import ca.gbc.comp3095.cookbook.model.User;
@@ -10,18 +19,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-@RequestMapping("/users")
-@Controller
+@RequestMapping("/users") // Map controller to /users path in URL
+@Controller // Annotates this class as a Controller to be managed by Spring Boot
 public class UserController {
 
+    // Dependencies
     private final UserService userService;
     private HttpSession newSession;
 
+    // Constructor Dependency Injection
     public UserController(UserService userService) {
         this.userService = userService;
         this.newSession = null;
     }
 
+    // adds all users to model.attribute & returns /users/index
     @RequestMapping({"", "/", "/index.html"})
     public String listUsers(Model model){
 
@@ -29,6 +41,8 @@ public class UserController {
         return "/users/index";
     }
 
+    // maps method to /users/login path
+    // adds new user object as attribute to model and returns /users/login
     @RequestMapping("/login")
     public String login(Model model) {
 
@@ -36,27 +50,30 @@ public class UserController {
         return "/users/login";
     }
 
+    // maps method to /users/process_login (POST)
+    // invalidates session, checks if user object equals to a user in the database
+    // if true it gets a session & sets the user object as a session attribute
+    // if false returns to login page
     @PostMapping({"/process_login"})
     public String process_login(User user, HttpSession session, HttpServletRequest request) {
 
         session.invalidate();
         boolean checkBool = userService.checkCredentials(user);
-        System.out.println("Check Boolean: " + checkBool); // Check
 
         if (checkBool) {
-            // Create new session & set user as the attribute
+            // get session & set user as the attribute
             newSession = request.getSession();
             newSession.setAttribute("user", user);
 
             // Redirect to Recipes (App)
-            System.out.println("Redirect to Profile"); // Check
-            return "redirect:/recipes";
+            return "redirect:/recipes/index";
         } else {
-            System.out.println("Redirect to Login"); // Check
             return "redirect:/users/login";
         }
     }
 
+    // maps method to /users/logout path
+    // invalidates session to clear attributes, sets newSession to null. and returns /users/logout
     @RequestMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
