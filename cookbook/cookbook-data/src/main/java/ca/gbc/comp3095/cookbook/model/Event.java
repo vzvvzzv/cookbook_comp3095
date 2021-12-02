12450a5c924a09260655f8fc8b6fe332b2e90e90
Chi Calvin Nguyen <chicalvin.nguyen@georgebrown.ca>
2021-12-02 02:39:28 -0500

    Issue 13 - Event Data Model Created Data Model for Event Created Relationships with User (One User to Many Events) Created Service & Service Map for Event Created Repository for Event TO DO: Business Logic (SERVICES) & CRUD OPERATIONS (REPOSITORY & SERVICE), Events Controller

---
+++ cookbook/cookbook-data/src/main/java/ca/gbc/comp3095/cookbook/model/Event.java
@@ -0,0 +1,57 @@
+package ca.gbc.comp3095.cookbook.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "events")
+public class Event extends BaseEntity{
+
+    @Id // Id - primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // PROPERTIES
+    private String eventName;
+
+    @Lob // annotation tells database to store the property as a large object
+    private String eventDetails;
+
+    // RELATIONSHIPS
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User eventUser;
+
+    // GETTERS & SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(String eventDetails) {
+        this.eventDetails = eventDetails;
+    }
+
+    public User getEventUser() {
+        return eventUser;
+    }
+
+    public void setEventUser(User eventUser) {
+        this.eventUser = eventUser;
+    }
+}
